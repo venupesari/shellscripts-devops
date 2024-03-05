e08b2e460d23251478c3fd06d1cfd9c42afe9307
venupesari <venupesari@gmail.com>
2024-03-05 05:52:04 +0300

    This is my first commit

---
+++ tomcat-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This shell script is to install tomcat
+Tomcat_version = 9.0.86
+# install java
+yum install java -y
+cd /tmp
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.zip
+unzip -q apache-tomcat-9.0.86.zip -d /opt/
+chmod -R 755 /opt/apache-tomcat-9.0.86.zip
