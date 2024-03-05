3e7d4f09733b66fc8a61b6ea6c79ee40357d4812
venupesari <venupesari@gmail.com>
2024-03-05 06:03:14 +0300

    This is my second commit

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -7,3 +7,4 @@ cd /tmp
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.86/bin/apache-tomcat-9.0.86.zip
 unzip -q apache-tomcat-9.0.86.zip -d /opt/
 chmod -R 755 /opt/apache-tomcat-9.0.86.zip
+#This is new line
